78e89024253c772f570df5eb4dc2d6e48984e4d8
Borja Garcia <debuti@gmail.com>
2024-06-13 11:28:05 +0200

    Added support for multiple calls in a row

--- umock/include/umock.h
+++ umock/include/umock.h
@@ -1,8 +1,6 @@
 #ifndef _umock_h_
 #define _umock_h_
 
-#define USE_ONCE (1)
-
 #define MOCKNAME(fn, name) fn##name
 
 /** Use a mock variant only once. Other calls will be directed
@@ -12,10 +10,36 @@
  * @param name The variant of the mock
  *
  */
-#define MOCKUSEONCE(fn, name)           \
-    do {                                \
-        fn##_impl = MOCKNAME(fn, name); \
-        fn##_flags |= USE_ONCE;         \
+#define MOCKUSEONCE(fn, name)                            \
+    do {                                                 \
+        fn##_idx = 0;                                    \
+        fn##_size = 2;                                   \
+        fn##_impl = (fn##_fn*) calloc(2, sizeof(void*)); \
+        fn##_impl[0] = MOCKNAME(fn, name);               \
+    } while (0)
+
+/** Setup the environment to use
+ *
+ * @param fn The name of the fn to be mocked
+ * @param n The number of expected invocations
+ *
+ */
+#define MOCKSETMANY(fn, n)                               \
+    do {                                                 \
+        fn##_idx = 0;                                    \
+        fn##_size = n;                                   \
+        fn##_impl = (fn##_fn*) calloc(n, sizeof(void*)); \
+    } while (0)
+
+/** Setup the environment to use
+ *
+ * @param fn The name of the fn to be mocked
+ * @param n The number of expected invocations
+ *
+ */
+#define MOCKSETIDX(fn, name, i)            \
+    do {                                   \
+        fn##_impl[i] = MOCKNAME(fn, name); \
     } while (0)
 
 /** Use a mock variant every time the mocked fn gets called
@@ -24,10 +48,12 @@
  * @param name The variant of the mock
  *
  */
-#define MOCKUSEALWAYS(fn, name)         \
-    do {                                \
-        fn##_impl = MOCKNAME(fn, name); \
-        fn##_flags &= ~USE_ONCE;        \
+#define MOCKUSEALWAYS(fn, name)                          \
+    do {                                                 \
+        fn##_idx = 0;                                    \
+        fn##_size = 1;                                   \
+        fn##_impl = (fn##_fn*) calloc(1, sizeof(void*)); \
+        fn##_impl[0] = MOCKNAME(fn, name);               \
     } while (0)
 
 /** Create a new mock variant
@@ -39,22 +65,26 @@
  * @param param The list of parameters of the fn to be mocked
  *
  */
-#define MOCK(ret, fn, name, param)   \
-    extern unsigned char fn##_flags; \
-    extern ret(*fn##_impl) param;    \
-    ret MOCKNAME(fn, name) param;    \
+#define MOCK(ret, fn, name, param) \
+    typedef ret(*fn##_fn)param;    \
+    extern unsigned int fn##_idx;  \
+    extern unsigned int fn##_size; \
+    extern fn##_fn * fn##_impl;    \
+    ret MOCKNAME(fn, name) param;  \
     ret MOCKNAME(fn, name) param
 
-#define MOCKENTRY(name, testsignature, callargs)             \
-    unsigned char name##_flags = 0;                          \
-    name##_fn name##_impl = ((void *)0);                     \
-    testsignature;                                           \
-    testsignature                                            \
-    {                                                        \
-        name##_fn impl = (name##_impl ? name##_impl : name); \
-        if (name##_flags & USE_ONCE)                         \
-            name##_impl = ((void *)0);                       \
-        return impl callargs;                                \
+
+#define MOCKENTRY(name, testsignature, callargs)                                                            \
+    name##_fn * name##_impl = ((void *)0);                                                                  \
+    unsigned int name##_idx = 0;                                                                            \
+    unsigned int name##_size = 0;                                                                           \
+    testsignature;                                                                                          \
+    testsignature                                                                                           \
+    {                                                                                                       \
+        name##_fn impl = (name##_impl ? (name##_impl[name##_idx] ? name##_impl[name##_idx] : name) : name); \
+        if ((name##_idx + 1) < name##_size)                                                                 \
+            name##_idx++;                                                                                   \
+        return impl callargs;                                                                               \
     }
 
 #endif
